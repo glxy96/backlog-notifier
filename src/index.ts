a38f64600eae834c757f895a7853d256e70b79e7
glxy96 <173794347+glxy96@users.noreply.github.com>
2025-02-12 12:31:08 +0900

    雛形の作成

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,35 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+export interface Env {
+  SLACK_BOT_API_KEY: string;
+  BACKLOG_API_KEY: string;
+  BACKLOG_SPACE_ID: string;
+  BACKLOG_PROJECT_ID: string;
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+    try {
+      // 1. Backlogのチケット取得
+
+      // 2. 送信メッセージの構築処理
+
+      // 3. Slack Botによるメッセージ送信処理
+    } catch (error) {
+      console.error("Error:", error);
+      return new Response("Internal Server Error", { status: 500 });
+    }
 	},
 } satisfies ExportedHandler<Env>;
+
+/**
+ * チケット取得処理
+ * Backlog APIはcurlでjson形式のチケット一覧を返す。
+ * クエリパラメータの指定で条件に合致するチケットに絞って取得できる。
+ */
+
+/**
+ * メッセージ構築処理
+ * fetchBacklogTicketsで取得したチケットからメッセージを構築する。
+ */
+
+/**
+ * メッセージ送信処理
+ */
